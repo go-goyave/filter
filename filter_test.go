fd20ea644d7b862cc4b048e1b66170df4ac8d7f1
SystemGlitch <jeremy.la@outlook.fr>
2021-08-17 16:16:34 +0200

    Test SQLEscape + getTableName

--- filter_test.go
+++ filter_test.go
@@ -1,11 +1,47 @@
 package filter
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+	"gorm.io/gorm/utils/tests"
 )
 
-func TestFilter(t *testing.T) {
-	assert.True(t, true)
+type TestModel struct {
+	Name string
+	ID   uint
+}
+
+func TestSQLEscape(t *testing.T) {
+	tx := &gorm.DB{Config: &gorm.Config{
+		Dialector: tests.DummyDialector{},
+	}}
+	assert.Equal(t, "`name`", SQLEscape(tx, "name"))
+}
+
+func TestGetTableName(t *testing.T) {
+	tx := &gorm.DB{
+		Config:    &gorm.Config{Dialector: tests.DummyDialector{}},
+		Statement: &gorm.Statement{},
+	}
+	tx.Statement.DB = tx
+
+	assert.Empty(t, getTableName(tx))
+
+	tx = tx.Table("users")
+
+	assert.Equal(t, "users.", getTableName(tx))
+
+	tx, _ = gorm.Open(tests.DummyDialector{}, nil)
+	tx = tx.Model(&TestModel{})
+
+	assert.Equal(t, "test_models.", getTableName(tx))
+
+	assert.Panics(t, func() {
+		tx, _ = gorm.Open(tests.DummyDialector{}, nil)
+		tx = tx.Model(1)
+		fmt.Println(getTableName(tx))
+	})
 }

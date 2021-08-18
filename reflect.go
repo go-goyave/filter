8405dcd31bdf959ac41bd31f5cc5506d40287f8d
SystemGlitch <jeremy.la@outlook.fr>
2021-08-18 14:29:47 +0200

    Add reflect operations for column names and relations

---
+++ reflect.go
@@ -0,0 +1,130 @@
+package filter
+
+import (
+	"reflect"
+	"strings"
+
+	"gorm.io/gorm"
+)
+
+// TODO cache reflect operations
+
+type modelIdentity struct {
+	Columns   map[string]column
+	Relations map[string]*modelIdentity
+}
+
+type column struct {
+	Name string
+	Tag  reflect.StructTag
+}
+
+func (i *modelIdentity) promote(identity *modelIdentity, prefix string) {
+	for k, v := range identity.Columns {
+		i.Columns[prefix+k] = v
+	}
+	for k, v := range identity.Relations {
+		i.Relations[prefix+k] = v
+	}
+}
+
+func parseModel(db *gorm.DB, model interface{}) *modelIdentity {
+	t := reflect.TypeOf(model)
+	return parseIdentity(db, t, []reflect.Type{t})
+}
+
+func parseIdentity(db *gorm.DB, t reflect.Type, parents []reflect.Type) *modelIdentity {
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+	if t.Kind() != reflect.Struct || checkCycle(t, parents) {
+		return nil
+	}
+	identity := &modelIdentity{
+		Columns:   make(map[string]column, 10),
+		Relations: make(map[string]*modelIdentity, 5),
+	}
+	count := t.NumField()
+
+	for i := 0; i < count; i++ {
+		field := t.Field(i)
+		fieldType := field.Type
+		if fieldType.Kind() == reflect.Ptr {
+			fieldType = fieldType.Elem()
+		}
+
+		switch fieldType.Kind() {
+		case reflect.Struct:
+			parents = append(parents, t)
+			if field.Anonymous {
+				// Promoted fields
+				i := parseIdentity(db, fieldType, parents)
+				if i == nil {
+					continue
+				}
+				identity.promote(i, "")
+			} else if i := parseIdentity(db, fieldType, parents); i != nil {
+				if prefix, ok := getEmbeddedInfo(field); ok {
+					identity.promote(i, prefix)
+				} else {
+					// "belongs to" / "has one" relation
+					identity.Relations[field.Name] = i
+				}
+			}
+		case reflect.Slice:
+			// "has many" relation
+			parents = append(parents, t)
+			if i := parseIdentity(db, fieldType.Elem(), parents); i != nil {
+				identity.Relations[field.Name] = i
+			}
+		default:
+			identity.Columns[columnName(db, field)] = column{
+				Name: field.Name,
+				Tag:  field.Tag,
+			}
+		}
+	}
+	return identity
+}
+
+func columnName(db *gorm.DB, field reflect.StructField) string {
+	for _, t := range strings.Split(field.Tag.Get("gorm"), ";") { // Check for gorm column name override
+		if strings.HasPrefix(t, "column") {
+			i := strings.Index(t, ":")
+			if i == -1 || i+1 >= len(t) {
+				// Invalid syntax, fallback to auto-naming
+				break
+			}
+			return strings.TrimSpace(t[i+1:])
+		}
+	}
+
+	return db.NamingStrategy.ColumnName("", field.Name)
+}
+
+func getEmbeddedInfo(field reflect.StructField) (string, bool) {
+	embedded := false
+	embeddedPrefix := ""
+	for _, t := range strings.Split(field.Tag.Get("gorm"), ";") { // Check for gorm column name override
+		if t == "embedded" {
+			embedded = true
+		} else if strings.HasPrefix(t, "embeddedPrefix") {
+			i := strings.Index(t, ":")
+			if i == -1 || i+1 >= len(t) {
+				continue
+			}
+			embeddedPrefix = strings.TrimSpace(t[i+1:])
+		}
+	}
+
+	return embeddedPrefix, embedded
+}
+
+func checkCycle(t reflect.Type, parents []reflect.Type) bool {
+	for _, v := range parents {
+		if t == v {
+			return true
+		}
+	}
+	return false
+}
---
+++ reflect_test.go
@@ -0,0 +1,113 @@
+package filter
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+	"gorm.io/gorm/utils/tests"
+)
+
+func TestParseModel(t *testing.T) {
+	type TestRelation struct {
+		Name string
+	}
+	type Promoted struct {
+		Email string `gorm:"column:email_address"`
+	}
+	type PromotedPtr struct {
+		Promoted
+	}
+	type PromotedRelation struct {
+		PromotedRelation TestRelation
+	}
+	type TestModel struct {
+		Promoted
+		*PromotedPtr
+		PromotedRelation
+		Str       string `gorm:"column:"`
+		Relation  *TestRelation
+		Relations []*TestRelation
+		ID        uint
+	}
+
+	db, _ := gorm.Open(&tests.DummyDialector{}, nil)
+	identity := parseModel(db, &TestModel{})
+
+	relation := &modelIdentity{
+		Columns: map[string]column{
+			"name": {Name: "Name", Tag: ""},
+		},
+		Relations: map[string]*modelIdentity{},
+	}
+	expected := &modelIdentity{
+		Columns: map[string]column{
+			"id":            {Name: "ID", Tag: ""},
+			"str":           {Name: "Str", Tag: `gorm:"column:"`},
+			"email_address": {Name: "Email", Tag: `gorm:"column:email_address"`},
+		},
+		Relations: map[string]*modelIdentity{
+			"Relation":         relation,
+			"Relations":        relation,
+			"PromotedRelation": relation,
+		},
+	}
+	assert.Equal(t, expected, identity)
+}
+
+type TestRelationCycle struct {
+	Parent *TestModelRelationCycle
+}
+type TestModelRelationCycle struct {
+	*TestModelRelationCycle
+	Relation *TestRelationCycle
+}
+
+func TestParseModelRelationCycle(t *testing.T) {
+	db, _ := gorm.Open(&tests.DummyDialector{}, nil)
+	identity := parseModel(db, &TestModelRelationCycle{})
+	expected := &modelIdentity{
+		Columns: map[string]column{},
+		Relations: map[string]*modelIdentity{
+			"Relation": {
+				Columns:   map[string]column{},
+				Relations: map[string]*modelIdentity{},
+			},
+		},
+	}
+	assert.Equal(t, expected, identity)
+}
+
+func TestParseModelEmbeddedStruct(t *testing.T) {
+	type TestEmbed struct {
+		Name string
+	}
+	type TestModelEmbedded struct {
+		Embed TestEmbed `gorm:"embedded;embeddedPrefix:embed_"`
+	}
+
+	db, _ := gorm.Open(&tests.DummyDialector{}, nil)
+	identity := parseModel(db, &TestModelEmbedded{})
+	expected := &modelIdentity{
+		Columns: map[string]column{
+			"embed_name": {Name: "Name", Tag: ""},
+		},
+		Relations: map[string]*modelIdentity{},
+	}
+	assert.Equal(t, expected, identity)
+}
+
+func TestGetEmbeddedInfoInvalidSyntax(t *testing.T) {
+	field := reflect.StructField{
+		Tag: `gorm:"embedded;embeddedPrefix:"`,
+	}
+
+	prefix, ok := getEmbeddedInfo(field)
+	assert.Empty(t, prefix)
+	assert.True(t, ok)
+}
+
+func TestParseNilModel(t *testing.T) {
+	assert.Nil(t, parseModel(nil, 1))
+}

f2b0ff89691ef2899347a63340873a3643c89143
SystemGlitch <jeremy.la@outlook.fr>
2021-08-20 13:38:31 +0200

    Test filter.Where

--- filter_test.go
+++ filter_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 	"gorm.io/gorm/utils/tests"
 )
 
@@ -44,4 +45,42 @@ func TestGetTableName(t *testing.T) {
 		tx = tx.Model(1)
 		fmt.Println(getTableName(tx))
 	})
+}
+
+func TestFilterWhere(t *testing.T) {
+	db, _ := gorm.Open(&tests.DummyDialector{}, nil)
+	filter := &Filter{Field: "name", Args: []string{"val1"}}
+	db = filter.Where(db, "name = ?", "val1")
+	expected := map[string]clause.Clause{
+		"WHERE": {
+			Name: "WHERE",
+			Expression: clause.Where{
+				Exprs: []clause.Expression{
+					clause.Expr{SQL: "name = ?", Vars: []interface{}{"val1"}},
+				},
+			},
+		},
+	}
+	assert.Equal(t, expected, db.Statement.Clauses)
+}
+
+func TestFilterWhereOr(t *testing.T) {
+	db, _ := gorm.Open(&tests.DummyDialector{}, nil)
+	filter := &Filter{Field: "name", Args: []string{"val1"}, Or: true}
+	db = filter.Where(db, "name = ?", "val1")
+	expected := map[string]clause.Clause{
+		"WHERE": {
+			Name: "WHERE",
+			Expression: clause.Where{
+				Exprs: []clause.Expression{
+					clause.OrConditions{
+						Exprs: []clause.Expression{
+							clause.Expr{SQL: "name = ?", Vars: []interface{}{"val1"}},
+						},
+					},
+				},
+			},
+		},
+	}
+	assert.Equal(t, expected, db.Statement.Clauses)
 }
